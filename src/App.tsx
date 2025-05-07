3e0d4f0dce7ce81ce9e629fe079adbb179fdfd9d
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-05-07 10:32:21 +0200

    feat: hold numbers and provide feedback when user has won

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {nanoid} from 'nanoid'
 function App() {
 
     const [dice, setDice] = React.useState(generateAllNewDice())
+    const [hasWon, setHasWon] = React.useState(false);
 
     function generateAllNewDice() {
         const newDice = [];
@@ -17,21 +18,40 @@ function App() {
     }
 
     function rollDice() {
-        setDice(generateAllNewDice)
+        setDice(prevState => {
+            const newDice = prevState.map(die => {
+                if (die.isHeld) {
+                    return die;
+                } else {
+                    const randomDice = Math.ceil(Math.random() * 6);
+                    return {...die, value: randomDice}
+                }
+            });
+            const allHeld = newDice.every(die => die.isHeld);
+            if (allHeld) {
+                setHasWon(true)
+                setTimeout(newGame, 2000)
+            }
+            return newDice
+        });
+    }
+
+    function newGame() {
+        setDice(generateAllNewDice())
+        setHasWon(false)
     }
 
     function toggleHold(id: string) {
         setDice(prevState =>
             prevState.map(die => {
                 if (die.id === id) {
-                    return { ...die, isHeld: !die.isHeld };
+                    return {...die, isHeld: !die.isHeld};
                 } else {
                     return die;
                 }
             })
         );
     }
-
 
 
     const ValueMapped = dice.map((dieValue) => (
@@ -45,9 +65,13 @@ function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {ValueMapped}
             </div>
+
+            {hasWon && <p className={"win-message"}>You've won congratulations</p>}
 
             <button className={"dice-button"} onClick={rollDice}>Roll</button>
         </main>
